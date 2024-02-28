222cbec71cae63924c5cb99785af1eb06d5da180
Emine <emineclsknn17@gmail.com>
2024-02-28 12:32:04 +0300

    DbContext created

---
+++ src/Infrastructure/Data/WatchWorldContext.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class WatchWorldContext : DbContext
+    {
+        public WatchWorldContext(DbContextOptions<WatchWorldContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            //oluşturacağımız  configirasyon dosyalarını implementasyonnu sağlıyor
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
