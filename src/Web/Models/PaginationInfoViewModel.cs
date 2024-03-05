c5fbbcff99960051926988be43e9bf1f40e06e02
Emine <emineclsknn17@gmail.com>
2024-03-05 10:49:23 +0300

    pagination info

---
+++ src/Web/Models/PaginationInfoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Web.Models
+{
+    public class PaginationInfoViewModel
+    {
+        public int PageId { get; set; }
+        public int TotalItems { get; set; }
+        public int ItemsOnPage { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / Constants.ITEMS_PER_PAGE);
+
+        public bool HasPrevious => PageId > 1;
+
+        public bool HasNext => PageId < TotalPages;
+
+
+        public int RangeStart => (PageId - 1) * Constants.ITEMS_PER_PAGE + 1;
+        public int RageEnd => RangeStart + ItemsOnPage -1;
+
+
+    }
+}
