5b1f29ffbe981d5afae1f6be485bf44ca76785f7
Emine <emineclsknn17@gmail.com>
2024-03-12 10:53:47 +0300

    migrations

--- src/Infrastructure/Data/WatchWorldContext.cs
+++ src/Infrastructure/Data/WatchWorldContext.cs
@@ -22,6 +22,12 @@ namespace Infrastructure.Data
 
         public DbSet<BasketItem> BasketItems { get; set; }
 
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             
