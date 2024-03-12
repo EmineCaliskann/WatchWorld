1c2b506c28d2c46b5dc07a6b92a9c676577170a4
Emine <emineclsknn17@gmail.com>
2024-03-12 10:06:03 +0300

    ApplicationCore Order

---
+++ src/ApplicationCore/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string BuyerId { get; set; } = null!;
+
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public Address ShipToAddrress { get; set; } = null!;
+
+        public List<OrderItem> Items { get; set; } = [];
+    }
+}
